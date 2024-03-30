4cb4d38f83bb91db5f8930a7db009f353c34d6e8
MrTort <ohnych.t@gmail.com>
2024-03-30 10:20:42 +0200

    conected data base

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const PORT = 3000;
 const app = express();
 
-app.use();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json);
+
+mongoose
+    .connect(
+        'mongodb+srv://ohnycht:CLa6PbJ5Fg1p0d2B@cluster0.f5xp95z.mongodb.net/'
+    )
+    .then(() => console.log('connected'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log('work well');
