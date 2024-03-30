3f77535865d4456b51e8bb66cd190b0f8ce47db0
MrTort <ohnych.t@gmail.com>
2024-03-30 10:00:28 +0200

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = 3000;
+const app = express();
+
+app.use();
+
+app.listen(PORT, () => {
+    console.log('work well');
+});
